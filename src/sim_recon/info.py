ffeb5a4d78600cb8a1bf1ebc5f42db44c22b7c46
Thomas Fish <thomas.fish@diamond.ac.uk>
2024-08-05 18:29:59 +0100

    `__version__`: make mypy happy

--- src/sim_recon/info.py
+++ src/sim_recon/info.py
@@ -1,6 +1,8 @@
 from importlib import metadata
 
+
+__version__: str | None = None
 try:
     __version__ = metadata.metadata("PySIMRecon")["version"]
 except metadata.PackageNotFoundError:
-    __version__ = None
+    pass
