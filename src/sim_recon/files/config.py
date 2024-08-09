301e884dcf412f52e56f28709c5678de988f61c8
Thomas Fish <thomas.fish@diamond.ac.uk>
2024-08-09 17:46:09 +0100

    Remove unused import

--- src/sim_recon/files/config.py
+++ src/sim_recon/files/config.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 import logging
 from pathlib import Path
-from os.path import abspath
 from configparser import RawConfigParser
 from typing import TYPE_CHECKING
 
