084eb61e97e701d10108bfef1604f968dd81f89f
Thomas Fish <thomas.fish@diamond.ac.uk>
2024-11-06 13:09:36 +0000

    Create `sim_reconstruct_single`
    Has ability to set processing directory directly (not the parent directory)
    Can set the multiprocessing pool to be used

--- src/sim_recon/__init__.py
+++ src/sim_recon/__init__.py
@@ -2,8 +2,13 @@ from .info import __version__
 
 
 if __name__ == "__main__":
-    from .main import sim_reconstruct, sim_psf_to_otf
+    from .main import sim_reconstruct, sim_psf_to_otf, sim_reconstruct_single
 
-    __all__ = ["__version__", "sim_reconstruct", "sim_psf_to_otf"]
+    __all__ = [
+        "__version__",
+        "sim_reconstruct",
+        "sim_reconstruct_single",
+        "sim_psf_to_otf",
+    ]
 else:
     __all__ = ["__version__"]
